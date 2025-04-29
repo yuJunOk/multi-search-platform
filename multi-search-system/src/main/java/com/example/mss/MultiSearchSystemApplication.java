a6c8a2d33c211ab1af40c1b13e519a8ffcd50ef9
pengYuJun <1375841038@qq.com>
2025-04-29 22:19:30 +0800

    add: 初始化平台后端

---
+++ multi-search-system/src/main/java/com/example/mss/MultiSearchSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.mss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiSearchSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiSearchSystemApplication.class, args);
+    }
+
+}
