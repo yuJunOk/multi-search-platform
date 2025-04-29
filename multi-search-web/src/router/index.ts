a1ff4920d34fc2b53bba55816a2f0b82bc75a3f8
pengYuJun <1375841038@qq.com>
2025-04-30 02:46:01 +0800

    add: 初步进行搜索页面开发

--- multi-search-web/src/router/index.ts
+++ multi-search-web/src/router/index.ts
@@ -1,20 +1,16 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import IndexView from "../views/IndexView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    name: "index",
+    component: IndexView,
   },
+  // 动态路由
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "/:category",
+    component: IndexView,
   },
 ];
 
