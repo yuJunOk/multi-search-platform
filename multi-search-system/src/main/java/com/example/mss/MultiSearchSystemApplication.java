a8af10759051f4c08a42f932f10247e9aa06ddc9
pengYuJun <1375841038@qq.com>
2025-04-29 23:12:45 +0800

    update: 添加author

--- multi-search-system/src/main/java/com/example/mss/MultiSearchSystemApplication.java
+++ multi-search-system/src/main/java/com/example/mss/MultiSearchSystemApplication.java
@@ -3,6 +3,9 @@ package com.example.mss;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author pengYuJun
+ */
 @SpringBootApplication
 public class MultiSearchSystemApplication {
 
