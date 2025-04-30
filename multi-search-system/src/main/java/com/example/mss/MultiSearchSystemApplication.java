e7d0e7249cc9f05b51e6ad6720744b07cd5746cb
pengYuJun <1375841038@qq.com>
2025-04-30 14:46:05 +0800

    update: 加上MapperScan

--- multi-search-system/src/main/java/com/example/mss/MultiSearchSystemApplication.java
+++ multi-search-system/src/main/java/com/example/mss/MultiSearchSystemApplication.java
@@ -1,11 +1,13 @@
 package com.example.mss;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author pengYuJun
  */
+@MapperScan("com.example.mss.mapper")
 @SpringBootApplication
 public class MultiSearchSystemApplication {
 
